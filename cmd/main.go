10583913298c6f6362619ccfb5339b04bf0fec32
Gopal Subbaratinam <gopal1852000@gmail.com>
2026-09-20 20:56:26 +0530

    added time

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
 )
 var global_var = 12
 
@@ -11,13 +12,14 @@ var global_var = 12
 func main(){
 
 	internal_val := 10
-
+	pid := os.Getpid()
 
 	fmt.Printf("Global: %v\n", &global_var)
-	fmt.Printf("internal: %v", &internal_val)
+	fmt.Printf("internal: %v\n", &internal_val)
+	fmt.Printf("pid: %v\n", pid)
 
 	for {
-		time.Sleep(3*time.Second)
+		time.Sleep(10*time.Second)
 		fmt.Println("----")
 	}
 }
