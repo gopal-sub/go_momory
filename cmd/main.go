ebd85fe37ef066ed4e9cf4c0f5238a2be98d4158
Gopal Subbaratinam <gopal1852000@gmail.com>
2026-09-20 20:53:37 +0530

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"time"
+)
 var global_var = 12
 
 
@@ -10,8 +12,12 @@ func main(){
 
 	internal_val := 10
 
-	
+
 	fmt.Printf("Global: %v\n", &global_var)
 	fmt.Printf("internal: %v", &internal_val)
 
+	for {
+		time.Sleep(3*time.Second)
+		fmt.Println("----")
+	}
 }
